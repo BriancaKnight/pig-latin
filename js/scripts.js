a93393a6f1758d1290aefd47efe493710dd41674
Brianca Knight <briancaknight@gmail.com>
2023-10-25 14:26:44 -0700

    Add functions to pass first 6 tests.
    Co-authored-by: Christopher Quattrocchi <chrisquattrocchi@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,35 @@ function firstLetterCheck(aString) {
   } else {
     let lowerString = aString.toLowerCase();
     let firstLetter = lowerString.slice(0, 1);
+    if (vowelStart(firstLetter)) {
+      console.log(lowerString.concat("way"));
+    } else {
+      
+    }
+    startWithQ(firstLetter);
     console.log(firstLetter);
+  }
+}
+
+const vowels = ["a", "e", "i", "o", "u"];
+function vowelStart(firstLetter){
+  if (firstLetter === "a" || firstLetter === "e" || firstLetter === "i" || firstLetter === "o" || firstLetter === "u") {
+    return true;
+    console.log("Starts with a vowel.");
+  } else {
+    return false;
+    console.log("Starts with a consonant");
   }
 }
 
 function onlyLetters(aString) {
   return /^[a-zA-Z]+$/.test(aString);
+}
+
+function startWithQ(firstLetter) {
+  if (firstLetter === "q") {
+    console.log("Starts with a Q");
+  } else {
+    console.log("does not start with a Q");
+  }
 }
