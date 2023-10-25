4d3e20c04dfa0f4d2fc571bdfe8c166aa2b2b2f0
Brianca Knight <briancaknight@gmail.com>
2023-10-25 13:57:42 -0700

    Add functions to pass first three tests.
    Co-authored-by: Christopher Quattrocchi <chrisquattrocchi@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
 function firstLetterCheck(aString) {
-  
+  if (!onlyLetters(aString)) {
+    console.log("Not alphabet");
+  } else {
+    let lowerString = aString.toLowerCase();
+    let firstLetter = lowerString.slice(0, 1);
+    console.log(firstLetter);
+  }
+}
+
+function onlyLetters(aString) {
+  return /^[a-zA-Z]+$/.test(aString);
 }
